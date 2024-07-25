6e28e02df38a6ec4d3c857e35782c13f9d0c3987
Don McCurdy <donmccurdy@cartodb.com>
2024-07-25 15:23:55 -0400

    Set up 'create.js' for each template

--- packages/create-angular/scripts/create.js
+++ packages/create-angular/scripts/create.js
@@ -1,1 +1,21 @@
-console.log('angular::create')
+#!/usr/bin/env node
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createProject } from '@carto/create-common';
+import meow from 'meow';
+
+const cli = meow(
+  `
+  Usage
+    $ yarn create @carto/angular <target-directory>
+  `,
+  {
+    importMeta: import.meta,
+    autoHelp: true,
+    autoVersion: true,
+  },
+);
+
+const templateDir = resolve(fileURLToPath(import.meta.url), '../..');
+const targetDir = cli.input.at(0) || '.';
+await createProject(templateDir, targetDir);
--- packages/create-vue/scripts/create.js
+++ packages/create-vue/scripts/create.js
@@ -1,1 +1,21 @@
-console.log('vue::create')
+#!/usr/bin/env node
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createProject } from '@carto/create-common';
+import meow from 'meow';
+
+const cli = meow(
+  `
+  Usage
+    $ yarn create @carto/vue <target-directory>
+  `,
+  {
+    importMeta: import.meta,
+    autoHelp: true,
+    autoVersion: true,
+  },
+);
+
+const templateDir = resolve(fileURLToPath(import.meta.url), '../..');
+const targetDir = cli.input.at(0) || '.';
+await createProject(templateDir, targetDir);
