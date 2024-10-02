15d3c9f573bf9f765183e42458b6c6f1a9e8c709
Don McCurdy <donmccurdy@cartodb.com>
2024-10-02 03:41:19 -0400

    chore(ci): Add script to generate project programmatically (#14)

---
+++ scripts/generate-ci-project.js
@@ -0,0 +1,31 @@
+import { createProjectFromConfig } from '@carto/create-common';
+import { resolve } from 'node:path';
+
+/**
+ * Helper script to generate test projects in CI. Given a template/framework
+ * name, a target project path, and an access token, creates a new project
+ * (without user interaction with the usual CLI) in that directory.
+ */
+
+const [_execPath, _scriptPath, template, projectPath, accessToken] =
+  process.argv;
+
+const TemplatePath = {
+  angular: './packages/create-angular',
+  react: './packages/create-react',
+  vue: './packages/create-vue',
+};
+
+if (!TemplatePath[template]) {
+  throw new Error(`Unexpected template, "${template}"`);
+}
+
+await createProjectFromConfig(
+  resolve(process.cwd(), TemplatePath[template]),
+  resolve(process.cwd(), projectPath),
+  {
+    title: `CI Test (${template})`,
+    authEnabled: false,
+    accessToken,
+  },
+);
