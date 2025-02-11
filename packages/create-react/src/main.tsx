02ebb93420be03f9143c0a43cb02e065bd6814b7
Don McCurdy <donmccurdy@cartodb.com>
2025-02-11 10:45:05 -0500

    chore(release): v0.1.4

--- packages/create-react/src/main.tsx
+++ packages/create-react/src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@carto/create-common/style.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// TODO: Restore <React.StrictMode /> wrapper after resolution of
+// https://github.com/visgl/deck.gl/issues/9379
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
