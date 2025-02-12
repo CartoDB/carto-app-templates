1b9fa30d93bc5abab64a88f47063dab3e506799e
Don McCurdy <donmccurdy@cartodb.com>
2025-02-12 14:02:13 -0500

    fixup! revert disabling react strict mode

--- packages/create-react/src/main.tsx
+++ packages/create-react/src/main.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@carto/create-common/style.css';
 
-// TODO: Restore <React.StrictMode /> wrapper after resolution of
-// https://github.com/visgl/deck.gl/issues/9379
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
